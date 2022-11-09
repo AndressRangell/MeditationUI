990ad3c794a63d3ba689bd8866eebdb298e80162
AndresRangelMeli <andres.rangel@mercadolibre.com.co>
2022-11-09 17:54:11 -0500

    Implemented bottom menu section

--- .idea/sonarlint/issuestore/index.pb
+++ .idea/sonarlint/issuestore/index.pb
@@ -1,13 +1,9 @@
 
 m
 =app/src/main/java/com/plcoding/meditationui/ui/theme/Color.kt,c/b/cba8e97c9267cb52bbba24e7035d0e4b9fe97912
-m
-=app/src/main/java/com/plcoding/meditationui/ui/theme/Shape.kt,0/f/0fc6b52dfbb9f83b98191b45625f7c7fe84ed611
 @
 app/build.gradle,f/4/f4a01d6a4fcb971362ec00a83903fd3902f52164
 l
 <app/src/main/java/com/plcoding/meditationui/ui/theme/Type.kt,3/1/311ac5443fe58c5737976f444dea55513ef1182b
-m
-=app/src/main/java/com/plcoding/meditationui/ui/theme/Theme.kt,4/c/4c4d9572228ff1de32580fdbc85279db74eaf851
 <
 build.gradle,f/0/f07866736216be0ee2aba49e392191aeae700a35
--- app/src/main/java/com/plcoding/meditationui/ui/HomeScreen.kt
+++ app/src/main/java/com/plcoding/meditationui/ui/HomeScreen.kt
@@ -32,6 +32,7 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.focus.focusModifier
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.Path
@@ -39,9 +40,11 @@ import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.plcoding.meditationui.BottomMenuContent
 import com.plcoding.meditationui.Feature
 import com.plcoding.meditationui.R
 import com.plcoding.meditationui.standardQuadFromTo
+import com.plcoding.meditationui.ui.theme.AquaBlue
 import com.plcoding.meditationui.ui.theme.Beige1
 import com.plcoding.meditationui.ui.theme.Beige2
 import com.plcoding.meditationui.ui.theme.Beige3
@@ -74,6 +77,10 @@ fun HomeScreen() {
             CurrentMeditation()
             FeatureSection(features = featuresList)
         }
+        BottomMenu(
+            items = itemMenuList,
+            modifier = Modifier.align(Alignment.BottomCenter)
+        )
     }
 }
 
@@ -153,7 +160,7 @@ fun CurrentMeditation(color: Color = LightRed) {
                 style = MaterialTheme.typography.h2
             )
             Text(
-                text = "Meditation / 3-10 min",
+                text = "Meditation • 3-10 min",
                 style = MaterialTheme.typography.body1,
                 color = TextWhite
             )
@@ -289,6 +296,75 @@ fun FeatureItems(feature: Feature) {
     }
 }
 
+@Composable
+fun BottomMenu(
+    items: List<BottomMenuContent>,
+    modifier: Modifier = Modifier,
+    activeHighlightColor: Color = ButtonBlue,
+    activeTextColor: Color = Color.White,
+    inactiveTextColor: Color = AquaBlue,
+    initialSelectedItemIndex: Int = 0
+) {
+    var selectedItemIndex by remember {
+        mutableStateOf(initialSelectedItemIndex)
+    }
+    Row(
+        horizontalArrangement = Arrangement.SpaceAround,
+        verticalAlignment = Alignment.CenterVertically,
+        modifier = modifier
+            .fillMaxWidth()
+            .background(DeepBlue)
+            .padding(15.dp)
+    ) {
+        items.forEachIndexed { index, item ->
+            BottomMenuItem(
+                item = item,
+                isSelected = index == selectedItemIndex,
+                activeHighlightColor = activeHighlightColor,
+                activeTextColor = activeTextColor,
+                inactiveTextColor = inactiveTextColor
+            ) {
+                selectedItemIndex = index
+            }
+        }
+    }
+}
+
+@Composable
+fun BottomMenuItem(
+    item: BottomMenuContent,
+    isSelected: Boolean = false,
+    activeHighlightColor: Color = ButtonBlue,
+    activeTextColor: Color = Color.White,
+    inactiveTextColor: Color = AquaBlue,
+    onItemClick: () -> Unit
+) {
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center,
+        modifier = Modifier.clickable { onItemClick() }
+    ) {
+        Box(
+            contentAlignment = Alignment.Center,
+            modifier = Modifier
+                .clip(RoundedCornerShape(10.dp))
+                .background(if (isSelected) activeHighlightColor else Color.Transparent)
+                .padding(10.dp)
+        ) {
+            Icon(
+                painter = painterResource(id = item.iconId),
+                contentDescription = item.title,
+                tint = if (isSelected) activeTextColor else inactiveTextColor,
+                modifier = Modifier.size(20.dp)
+            )
+        }
+        Text(
+            text = item.title,
+            color = if (isSelected) activeTextColor else inactiveTextColor
+        )
+    }
+}
+
 val featuresList = listOf(
     Feature(
         title = "Sleep meditation",
@@ -318,4 +394,12 @@ val featuresList = listOf(
         Beige2,
         Beige3
     )
+)
+
+val itemMenuList = listOf(
+    BottomMenuContent("Home", R.drawable.ic_home),
+    BottomMenuContent("Meditate", R.drawable.ic_bubble),
+    BottomMenuContent("Sleep", R.drawable.ic_moon),
+    BottomMenuContent("Music", R.drawable.ic_music),
+    BottomMenuContent("Profile", R.drawable.ic_profile)
 )
